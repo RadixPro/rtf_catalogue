356027bd6f1f6295d0885c5c37f00698a2a7edc2
jan <jan@kampherbeek.com>
2015-04-18 16:14:51 +0200

    Initial version as Maven Project.

---
+++ RtfCore/src/main/java/org/readthisfirst/cat/ReadThisFirstApplication.java
@@ -0,0 +1,12 @@
+package org.readthisfirst.cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadThisFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadThisFirstApplication.class, args);
+    }
+}
---
+++ RtfCore/src/test/java/org/readthisfirst/cat/ReadThisFirstApplicationTests.java
@@ -0,0 +1,18 @@
+package org.readthisfirst.cat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ReadThisFirstApplication.class)
+@WebAppConfiguration
+public class ReadThisFirstApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
