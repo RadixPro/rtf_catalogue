1dc1e8c239c290b3dccdf1442d49aca714e75ab4
jan <jan@kampherbeek.com>
2015-05-08 14:49:21 +0200

    Added DDL and empty application.

--- RtfCore/src/main/java/org/readthisfirst/cat/ReadThisFirstApplication.java
+++ RtfCore/src/main/java/org/readthisfirst/cat/ReadThisFirstApplication.java
@@ -1,12 +1,19 @@
 package org.readthisfirst.cat;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReadThisFirstApplication {
+public class ReadThisFirstApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ReadThisFirstApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println("Started...");
     }
 }
